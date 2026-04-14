edf2c7a5625e2a5b1b0bdcf9274f8c8e106b4e26
Pantera5641 <mazurov815@gmail.com>
2026-04-14 21:38:41 +0300

    добавлен метод для подсчета стоимости

--- src/logic/recordPage/recordPageLogic.cpp
+++ src/logic/recordPage/recordPageLogic.cpp
@@ -27,3 +27,26 @@ void RecordPageLogic::addRecordInDataBase(
         serviceProvided,
         "accepted");
 }
+
+QString RecordPageLogic::getPrice(const QString& serviceProvided)
+{
+    int amount {};
+    std::map <QString, int> servicesList {};
+
+    QFile file(QCoreApplication::applicationDirPath() + "/data/servicesList.txt");
+    file.open(QIODevice::ReadOnly | QIODevice::Text);
+    QTextStream stream(&file);
+    while (!stream.atEnd())
+    {
+        QString line = stream.readLine();
+        QStringList tokens = line.split(';');
+        servicesList.insert(std::make_pair(tokens.at(1), tokens.at(2).toInt()));
+    }
+
+    if (const auto pair = servicesList.find(serviceProvided); pair != servicesList.end())
+        amount+=pair->second;
+
+    file.close();
+
+    return QString::number(amount);
+}
