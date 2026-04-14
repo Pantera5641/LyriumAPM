09513522e2b93eaceea132c3719e5d8a06f5fd40
Pantera5641 <mazurov815@gmail.com>
2026-04-14 21:39:41 +0300

    Перенесено, добавлены все поля и метод для перевода в строку

---
+++ src/database/simpleRecord.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <QString>
+
+struct SimpleRecord
+{
+    QString id {};
+    QString clientFullName {};
+    QString clientShortName {};
+    QString phoneNumber {};
+    QString email {};
+    QString carBrand {};
+    QString carModel {};
+    QString comment {};
+    QString masterFullName {};
+    QString masterShortName {};
+    QString serviceProvided {};
+    QString price {};
+    QString status {};
+    QString date {};
+
+    [[nodiscard]]
+    QString toString() const;
+};
